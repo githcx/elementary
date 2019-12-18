8fac8d94c858f3265cb107d617e635aeea97cf7e
githcx <xnhcx@qq.com>
2019-12-19 02:45:51 +0800

    make it github friendly

--- async.cc
+++ async.cc
@@ -10,175 +10,175 @@ using namespace std;
 
 struct Job
 {
-		void (*worker)(void* arg);
-		void* arg;
+  void (*worker)(void* arg);
+  void* arg;
 };
 
 class AsyncJobPool
 {
-	public:
-		AsyncJobPool(int concur_ = 5):concur(concur_),cont(true){}
-		int concur;
-		bool cont;
-		mutex m;
-		condition_variable c;
-		queue<Job> jobs;
-		vector<shared_ptr<thread>> vpth;
+  public:
+    AsyncJobPool(int concur_ = 5):concur(concur_),cont(true){}
+    int concur;
+    bool cont;
+    mutex m;
+    condition_variable c;
+    queue<Job> jobs;
+    vector<shared_ptr<thread>> vpth;
 
-		void PushJob(Job j)
-		{
-			unique_lock<mutex> lck(m);
-			if( !cont ) return;
-			jobs.push(j);
-			c.notify_one();
-		}
+    void PushJob(Job j)
+    {
+      unique_lock<mutex> lck(m);
+      if( !cont ) return;
+      jobs.push(j);
+      c.notify_one();
+    }
 
-		shared_ptr<Job> PopJob()
-		{
-			unique_lock<mutex> lck(m);
-			while(jobs.empty())
-			{
-				if( !cont ) return NULL;
-				c.wait(lck);
-			}
+    shared_ptr<Job> PopJob()
+    {
+      unique_lock<mutex> lck(m);
+      while(jobs.empty())
+      {
+        if( !cont ) return NULL;
+        c.wait(lck);
+      }
 
-			Job ret = jobs.front(); jobs.pop();
-			return make_shared<Job>(ret);
-		}
+      Job ret = jobs.front(); jobs.pop();
+      return make_shared<Job>(ret);
+    }
 
-		void Start() {
-			for(int i = 0; i < concur; i++)
-			vpth.push_back( make_shared<thread>(
-				[&]{
-						while(true)
-						{
-							auto j = PopJob();
-							if( j == NULL )
-								break;
+    void Start() {
+      for(int i = 0; i < concur; i++)
+      vpth.push_back( make_shared<thread>(
+        [&]{
+            while(true)
+            {
+              auto j = PopJob();
+              if( j == NULL )
+                break;
 
-							j->worker(j->arg);
-						}
+              j->worker(j->arg);
+            }
 
-						if( !cont )
-						{
-							c.notify_all();
-						}
-					}
-			) );
-		}
+            if( !cont )
+            {
+              c.notify_all();
+            }
+          }
+      ) );
+    }
 
-		void Stop()
-		{
-			cont = false;
-			c.notify_all();
-			for(auto& pth : vpth)
-				pth->join();
-		}
+    void Stop()
+    {
+      cont = false;
+      c.notify_all();
+      for(auto& pth : vpth)
+        pth->join();
+    }
 };
 
 struct NotifiedArg
 {
-		condition_variable* c;
-		mutex* m;
-		atomic<int>* cnt;
-		Job j;
+    condition_variable* c;
+    mutex* m;
+    atomic<int>* cnt;
+    Job j;
 };
 
 void NotifiedWorker(void* nArg)
 {
-	shared_ptr<NotifiedArg> p_nArg((NotifiedArg*)nArg);
+  shared_ptr<NotifiedArg> p_nArg((NotifiedArg*)nArg);
 
-	p_nArg->j.worker(p_nArg->j.arg);  // do real job
+  p_nArg->j.worker(p_nArg->j.arg);  // do real job
 
-	unique_lock<mutex> lck(*(p_nArg->m));
-	--*(p_nArg->cnt);
-	p_nArg->c->notify_one();
+  unique_lock<mutex> lck(*(p_nArg->m));
+  --*(p_nArg->cnt);
+  p_nArg->c->notify_one();
 }
 
 class AsyncJobGroup
 {
-	public:
-		AsyncJobGroup(AsyncJobPool& pool_):pool(pool_){ cnt = 0; }
-		AsyncJobPool& pool;
-		condition_variable c;
-		mutex m;
-		atomic<int> cnt;
+  public:
+    AsyncJobGroup(AsyncJobPool& pool_):pool(pool_){ cnt = 0; }
+    AsyncJobPool& pool;
+    condition_variable c;
+    mutex m;
+    atomic<int> cnt;
 
-		void CommitJob(Job j)
-		{
-			cnt++;
-			auto notifiedArg = new NotifiedArg;
-			notifiedArg->c = &c;
-			notifiedArg->m = &m;
-			notifiedArg->cnt = &cnt;
-			notifiedArg->j = j;
-			Job pj = {&NotifiedWorker, notifiedArg};
-			pool.PushJob(pj);
-		}
+    void CommitJob(Job j)
+    {
+      cnt++;
+      auto notifiedArg = new NotifiedArg;
+      notifiedArg->c = &c;
+      notifiedArg->m = &m;
+      notifiedArg->cnt = &cnt;
+      notifiedArg->j = j;
+      Job pj = {&NotifiedWorker, notifiedArg};
+      pool.PushJob(pj);
+    }
 
-		void Wait()
-		{
-				unique_lock<mutex> lck(m);
-				while(cnt)
-				{
-					c.wait(lck);
-				}
+    void Wait()
+    {
+        unique_lock<mutex> lck(m);
+        while(cnt)
+        {
+          c.wait(lck);
+        }
 
-				return;
-		}
+        return;
+    }
 };
 
 void DecreaseCounter(void *arg)
 {
-		atomic<int>& n = *(atomic<int>*)arg;
+    atomic<int>& n = *(atomic<int>*)arg;
 
-		static mutex g_mtx;
-		unique_lock<mutex> lck(g_mtx);
-		cout << "Task id = " << n << " finished" << endl;
+    static mutex g_mtx;
+    unique_lock<mutex> lck(g_mtx);
+    cout << "Task id = " << n << " finished" << endl;
 
-		n--;
+    n--;
 }
 
 int main()
 {
-	atomic<int> n;
-	n = 5;
-	int data[20];
-	Job jarr[20];
-	for(int i = 0; i < 20; i++)
-	{
-		data[i] = i + 1;
-		jarr[i] = {&DecreaseCounter, &data[i]};
-	}
+  atomic<int> n;
+  n = 5;
+  int data[20];
+  Job jarr[20];
+  for(int i = 0; i < 20; i++)
+  {
+    data[i] = i + 1;
+    jarr[i] = {&DecreaseCounter, &data[i]};
+  }
 
-	AsyncJobPool pool;
-	
-	pool.Start();
+  AsyncJobPool pool;
+  
+  pool.Start();
 
-	thread t1(
-	[&]
-	{
-		AsyncJobGroup group(pool);
-		for(int i = 0; i < 10; i++)
-			group.CommitJob(jarr[i]);
-		group.Wait();
-	}
-	);
+  thread t1(
+  [&]
+  {
+    AsyncJobGroup group(pool);
+    for(int i = 0; i < 10; i++)
+      group.CommitJob(jarr[i]);
+    group.Wait();
+  }
+  );
 
-	thread t2(
-	[&]
-	{
-		AsyncJobGroup group(pool);
-		for(int i = 0; i < 10; i++)
-			group.CommitJob(jarr[i+10]);
-		group.Wait();
-	}
-	);
+  thread t2(
+  [&]
+  {
+    AsyncJobGroup group(pool);
+    for(int i = 0; i < 10; i++)
+      group.CommitJob(jarr[i+10]);
+    group.Wait();
+  }
+  );
 
-	t1.join();
-	t2.join();
+  t1.join();
+  t2.join();
 
-	pool.Stop();
+  pool.Stop();
 
-	return 0;
+  return 0;
 }
--- nonblock.cc
+++ nonblock.cc
@@ -20,77 +20,77 @@ using namespace std;
 
 int main(void)
 {
-	int sock_fd = -1; 
-	char buf[BUF_SIZE];
-	//建立sock_fd套接字
-	if((sock_fd = open("./chan", O_RDONLY | O_NONBLOCK))==-1)
-	{
-		perror("setsockopt");
-		return 1;
-	}
+  int sock_fd = -1; 
+  char buf[BUF_SIZE];
+  //建立sock_fd套接字
+  if((sock_fd = open("./chan", O_RDONLY | O_NONBLOCK))==-1)
+  {
+    perror("setsockopt");
+    return 1;
+  }
 
-	fd_set fdsr; //文件描述符集的定义
-	struct timeval tv;
-	tv.tv_sec = 30;
-	tv.tv_usec =0;
+  fd_set fdsr; //文件描述符集的定义
+  struct timeval tv;
+  tv.tv_sec = 30;
+  tv.tv_usec =0;
 
-	int total_bytes = 0;
-	
-	while(1)
-	{
-		//初始化文件描述符集合
-		//超时的设定
-		//如果文件描述符中有连接请求 会做相应的处理，实现I/O的复用 多用户的连接通讯
-		FD_ZERO(&fdsr); //清除描述符集
-		FD_SET(sock_fd,&fdsr); //把sock_fd加入描述符集
-		int ret = select(sock_fd +1,&fdsr,NULL,NULL,&tv);
-		if(ret < 0) //没有找到有效的连接 失败
-		{
-			perror("select error!\n");
-			break;
-		}
+  int total_bytes = 0;
+  
+  while(1)
+  {
+    //初始化文件描述符集合
+    //超时的设定
+    //如果文件描述符中有连接请求 会做相应的处理，实现I/O的复用 多用户的连接通讯
+    FD_ZERO(&fdsr); //清除描述符集
+    FD_SET(sock_fd,&fdsr); //把sock_fd加入描述符集
+    int ret = select(sock_fd +1,&fdsr,NULL,NULL,&tv);
+    if(ret < 0) //没有找到有效的连接 失败
+    {
+      perror("select error!\n");
+      break;
+    }
 
-		int total_read = 0;
-		//循环判断有效的连接是否有数据到达
-		for(int i=0;i<ret;i++)
-		{
-			if(FD_ISSET(sock_fd,&fdsr))
-			{
-				again:
-				ret = read(sock_fd, buf,sizeof(buf));
-				total_read += 1;
-				if(ret == 0) //客户端连接关闭，清除文件描述符集中的相应的位
-				{
-					close(sock_fd);
-					sock_fd = -1;
-					cout << "EOF received" << endl;
-					cout << "total bytes = " << total_bytes << endl;
-					return 0;
-				}
+    int total_read = 0;
+    //循环判断有效的连接是否有数据到达
+    for(int i=0;i<ret;i++)
+    {
+      if(FD_ISSET(sock_fd,&fdsr))
+      {
+        again:
+        ret = read(sock_fd, buf,sizeof(buf));
+        total_read += 1;
+        if(ret == 0) //客户端连接关闭，清除文件描述符集中的相应的位
+        {
+          close(sock_fd);
+          sock_fd = -1;
+          cout << "EOF received" << endl;
+          cout << "total bytes = " << total_bytes << endl;
+          return 0;
+        }
 
-				if( ret == -1 )
-				{
-					if( errno == EAGAIN)
-					{
-						cout << "read " << total_read << " times for this select" << endl;
-						continue;
-					}
-						
-					perror("exception");
-					return -1;
-				}
-				//否则有相应的数据发送过来 ，进行相应的处理
-				else
-				{
-					//for(int j = 0; j < ret; j++) putchar(buf[j]);
-					//putchar('\n');
-					total_bytes += ret;
-					goto again;
-				}
-			}
-		}
-	}
+        if( ret == -1 )
+        {
+          if( errno == EAGAIN)
+          {
+            cout << "read " << total_read << " times for this select" << endl;
+            continue;
+          }
+            
+          perror("exception");
+          return -1;
+        }
+        //否则有相应的数据发送过来 ，进行相应的处理
+        else
+        {
+          //for(int j = 0; j < ret; j++) putchar(buf[j]);
+          //putchar('\n');
+          total_bytes += ret;
+          goto again;
+        }
+      }
+    }
+  }
 
-	return 0;
+  return 0;
 }
 
